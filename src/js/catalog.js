e429e74cf5c44861f481d56f8beaf347393e2477
RyanHan0127 <ryanhanboy@gmail.com>
2022-02-17 19:15:27 -0800

    Refactor code
    - Move dynamo db operations to a class
    - Added more dependencies

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,41 +1,28 @@
-var AWS = require("aws-sdk");
-var dotenv = require("dotenv");
+import { Dynamo } from "./Dynamo.js"
 
-require("dotenv").config();
-
-AWS.config.update({
-	region: process.env.REGION,
-	accessKeyId: process.env.ACCESS_ID,
-    secretAccessKey: process.env.ACCESS_KEY,
-	endpoint: process.env.ENDPOINT
-});
-
-var docClient = new AWS.DynamoDB.DocumentClient();
+var docClient = null;
 
 window.onload = function() {
 	let filterButton = document.getElementById("filterButton");
 
 	filterButton.addEventListener("click", onClickFilter, false);
+
+	docClient = new Dynamo();
+	console.log(docClient)
 }
 
 async function onClickFilter() {
 	var productType = document.getElementById("items");
 	let divCatalog = document.getElementById("divCatalog");
 	var divLoading = document.getElementById("divLoading");
-	var params = makeFilterParam(productType.value);
 
-	try {
-		divCatalog.innerHTML = '';
-		divLoading.style.visibility = 'visible';
+	divCatalog.innerHTML = '';
+	divLoading.style.visibility = 'visible';
 
-		const resp = await docClient.query(params).promise();
-		console.log("Query succeeded.");
+	const resp = await docClient.queryTable("ProductCatalog", "ProductType-index", "ProductType", productType.value);
 
-		divLoading.style.visibility = 'hidden';
-		updateTable(divCatalog, resp.Items);
-	} catch (err) {
-		console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-	}
+	divLoading.style.visibility = 'hidden';
+	updateTable(divCatalog, resp.Items);
 }
 
 function updateTable(divCatalog, listOfProducts) {
@@ -43,7 +30,6 @@ function updateTable(divCatalog, listOfProducts) {
 
 	divCatalog.innerHTML = '';
 	listOfProducts.forEach(function(item) {
-		console.log(item);
 		let leftTag = createTag('div', 'left', null);
 		let rightTag = createTag('div', 'right', null);
 		let liTag = createTag('li', 'split', null);
@@ -54,7 +40,6 @@ function updateTable(divCatalog, listOfProducts) {
 					"Seller: " + item['Seller Name'], 
 					"Location: " + item['Location'], 
 					"Cost: " + item['Cost']]
-		console.log(info);
 
 		for (let i = 0; i < info.length; i++) {
 			let liTag = createTag('li', null, null);
@@ -62,8 +47,11 @@ function updateTable(divCatalog, listOfProducts) {
 			ulTagInfo.appendChild(liTag);
 		}
 
+		let ahref = createTag('a', null, null);
+		ahref.href = '#';
 		imgTag.src = "img/chair.jpeg";
-		leftTag.appendChild(imgTag);
+		ahref.appendChild(imgTag);
+		leftTag.appendChild(ahref);
 		rightTag.appendChild(ulTagInfo);
 
 		liTag.appendChild(leftTag);
@@ -72,7 +60,6 @@ function updateTable(divCatalog, listOfProducts) {
 		ulTagItem.appendChild(liTag);
 	});
 
-	console.log(ulTagItem);
 	divCatalog.appendChild(ulTagItem);
 	divCatalog.style.visibility = 'visible';
 }
@@ -86,20 +73,4 @@ function createTag(tagName, className, idName) {
 		tag.id = idName;
 	}
 	return tag;
-}
-
-function makeFilterParam(productType) {
-	let params = {
-		TableName : "ProductCatalog",
-		IndexName: "ProductType-index",
-		KeyConditionExpression: "#pt = :pt",
-		ExpressionAttributeNames:{
-			"#pt": "ProductType"
-		},
-		ExpressionAttributeValues: {
-			":pt": productType
-		}
-	};
-
-	return params;
 }
