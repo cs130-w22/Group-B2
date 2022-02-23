54d035b11047d1bfb5124026f4b0f517e519f9e6
RyanHan0127 <ryanhanboy@gmail.com>
2022-02-22 21:41:11 -0800

    Post Description Implementation
    - Added Post description implementation
      - Added DynamoDB put entry
      - Added Image upload to S3
    - Used crypto module to create ID
    - Files are now going to be uploaded in the directory of:
      - <ProductID>/<ImageID>

--- js/Dynamo.js
+++ js/Dynamo.js
@@ -11,7 +11,6 @@ export class Dynamo {
             endpoint: process.env.ENDPOINT
         });
         this.client = new AWS.DynamoDB.DocumentClient();
-        this.s3 = new AWS.S3();
     }
 
     makeQueryParam(tableName, indexName, attrName, attrVal) {
@@ -52,6 +51,23 @@ export class Dynamo {
         return params;
     }
 
+    makePutParam(productID, itemCost, itemName, imageID, itemCategory) {
+        let params = {
+            TableName: 'ProductCatalog',
+            Item: {
+                ProductID: String(productID),
+                Cost: String(itemCost),
+                UserID: 'cookies',
+                Location: 'cookies',
+                Product: String(itemName),
+                ImageID: String(imageID),
+                ProductType: String(itemCategory),
+                SellerName: 'cookies'
+            }
+        };
+        return params;
+    }
+
     queryTable(tableName, indexName, attrName, attrVal) {
         let params = this.makeQueryParam(tableName, indexName, attrName, attrVal);
         try {
@@ -73,71 +89,17 @@ export class Dynamo {
             const resp = this.client.get(params).promise();
             return resp;
         } catch (err) {
-            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            console.error("Unable to get. Error:", JSON.stringify(err, null, 2));
         }
     }
 
-    putTable(productID, itemCost, itemName, imageID, itemCategory) {
-        console.log("putting table");
-        var params = {
-            TableName: 'ProductCatalog',
-            Item: {
-                ProductID: String(productID),
-                Cost: String(itemCost),
-                UserID: 'cookies',
-                Location: 'cookies',
-                Product: String(itemName),
-                ImageID: String(imageID),
-                ProductType: String(itemCategory),
-                SellerName: 'cookies'
-            }
-        };
+    putTable(productID, itemCost, itemName, imageID, itemCategory) { 
+        let params = this.makePutParam(productID, itemCost, itemName, imageID, itemCategory);
         try {
-            this.client.put(params, function (err, data) {
-                if (err) console.log("error putting table");
-                else console.log("table put!");
-            });
+            const resp = this.client.put(params).promise();
+            return resp
         } catch (err) {
-            console.error("unable to put table");
+            console.error("Unable to put. Error:", JSON.stringify(err, null, 2));
         }
-        console.log("... leaving putTable func");
-    }
-
-    createS3Bucket(imageID){
-        console.log("creating bucket...")
-        var params = {
-            Bucket: String(imageID),
-            CreateBucketConfiguration: {
-                LocationConstraint: "us-west-1"
-            }
-        };
-        
-        try {
-            this.s3.createBucket(params, function (err, data) {
-                if (err) console.log("error creating bucket"); // an error occurred
-                else console.log("bucket created!");           // successful response
-            
-            });
-        } catch (err) {
-            console.error("unable to create s3 bucket");
-        }
-        console.log("... leaving createS3Bucket func");
-    }
-
-    putImage(imageID){
-        console.log("putting image...");
-        var params = {
-            Body: "data",
-            Bucket: imageID,
-        };
-        try{
-            this.s3.putObject(params, function (err, data) {
-                if (err) console.log("error putting image"); // an error occurred
-                else console.log("image put!");           // successful response
-            });
-        } catch(err) {
-            console.error("unable to put image: ", JSON.stringify(err, null, 2));
-        }
-        console.log("... leaving putImage func");
     }
 }
--- js/createPost.js
+++ js/createPost.js
@@ -1,20 +1,24 @@
 import { Dynamo } from "./Dynamo.js"
+import { S3Bucket } from "./S3Bucket.js"
+var crypto = require("crypto");
 
-var docClient = new Dynamo();
+var docClientS3 = null;
+var docClientDynamo = null;
 
-window.onload = function () {
+window.onload = function() {
     let createPostButton = document.getElementById("submit");
     createPostButton.addEventListener("click", validatePostCreation, false);
+
+    docClientS3 = new S3Bucket();
+    docClientDynamo = new Dynamo();
 }
-
-
 
 async function validatePostCreation() {
     var itemName = document.getElementById("item_name").value;
     var itemCost = document.getElementById("item_cost").value;
     var itemDescription = document.getElementById("item_description").value;
     var itemCategory = document.getElementById("category").value;
-    var image = document.getElementById("myFile").value;
+    var image = document.getElementById("myFile").files[0];
 
     var cost = new RegExp('([0-9])+\.?([0-9])*');
 
@@ -24,17 +28,10 @@ async function validatePostCreation() {
     }
 
     if (!cost.test(itemCost)) {
-        //console.log("uh");
         alert("please enter valid item cost");
         return false;
     } else {
-
-        if (itemCost.indexOf('.') == itemCost.length - 3 || itemCost.indexOf('.') == itemCost.length - 2 || itemCost.indexOf('.') == -1) {
-            // okay format
-            // $x.x
-            // $x.xx
-            // $x
-        } else {
+        if (!(itemCost.indexOf('.') == itemCost.length - 3 || itemCost.indexOf('.') == itemCost.length - 2 || itemCost.indexOf('.') == -1)) {
             alert("please enter valid item cost");
             return false;
         }
@@ -50,29 +47,33 @@ async function validatePostCreation() {
         return false;
     }
 
-    var productID = generateString(8);
-    var imageID = generateString(8);
-    docClient.putTable(productID, itemCost, itemName, (image+imageID), itemCategory);
-    console.log("1");
-    docClient.createS3Bucket((image+imageID));
-    console.log("2");
-    docClient.putImage((image+imageID));
-    console.log("3");
-    //docClient.putS3(image+imageID, image);
+    var productID = generateID(5);
+    var imageID = generateID(5);
+    const respDynamo = await docClientDynamo.putTable(productID, itemCost, itemName, imageID, itemCategory);
+    const respS3 = await getPresignedAndUpload(productID + "/" + imageID, image);
 
-}
-
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-function generateString(length) {
-    let result = "";
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    if (respDynamo['$response']['httpResponse']['statusCode'] == 200 && respS3['status'] == 200) {
+        window.alert("Post uploaded! Check the catalog to see your post!");
+    } else {
+        window.alert("Something went wrong...\nDynamo Status: " 
+        + respDynamo['$response']['httpResponse']['statusCode'] 
+        + "\nS3 Status: " + respS3);
     }
-
-    return result;
 }
 
+async function getPresignedAndUpload(directory, image) {
+    const url = await docClientS3.generateURL(directory);
+    const resp = await fetch(url, {
+        method: "PUT",
+        headers: {
+           "Content-Type": "multipart/form-data"
+        },
+        body: image
+    });
 
+    return resp;
+}
 
+function generateID(length) {
+    return crypto.randomBytes(length).toString('hex');
+}
