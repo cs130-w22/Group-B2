9bfb37f110c41a346acc29c67fb4cac30f2a1396
Jessica Bojorquez <jessicarbojorquez@gmail.com>
2022-03-04 02:05:01 -0800

    Added query for user profile information to display in user profile, added link to profile in other html files

--- js/myProfile.js
+++ js/myProfile.js
@@ -1,8 +1,11 @@
 /**
  * @file profile page logic 
  */
+import { utils } from "hash.js";
 import { Dynamo } from "./Dynamo.js"
 import { S3Bucket } from "./S3Bucket.js"
+import * as utilities from './utils.js'
+
 
 /**
  * S3 Object
@@ -15,9 +18,17 @@ var docClientS3 = null;
  */
 var docClientDynamo = null;
 
+var id = "a6079e34ec";
+
+
+
 window.onload = function(){
     docClientDynamo = new Dynamo(); 
     docClientS3 = new S3Bucket(); 
+    const response = getUserInfo(id);
+    console.log(response);
+    console.log("hi");
+    response.then(result => populateUserData(result));
 } 
 
 /**
@@ -61,4 +72,50 @@ async function removePostFromTable(userID, productID){
         window.alert("Something went wrong...\n");
     }
      
+}
+
+
+async function getUserInfo(userID){
+    const entry = await docClientDynamo.getTableEntry("UserInformation", userID);
+    return entry;
+}
+
+function populateUserData(result) {
+    result.Item.Address
+    let userInfoSquare = document.getElementById("userInfoSquare");
+    let userNameSquare = document.getElementById("userNameSquare");
+
+    let h2 = utilities.createTag('h', null, null);
+    let p  = utilities.createTag('p', null, null);
+    let p2 = utilities.createTag('p', null, null);
+    let p3 = utilities.createTag('p', null, null);
+    let p4 = utilities.createTag('p', null, null);
+    
+
+    let user_firstname = document.createTextNode(result.Item.FirstName);
+    let user_lastname = document.createTextNode(result.Item.LastName);
+    let user_email = document.createTextNode(result.Item.Email);
+    let user_phone = document.createTextNode(result.Item.PhoneNumber);
+    
+    h2.appendChild(user_firstname);
+    userNameSquare.appendChild(h2);
+
+    p.appendChild(user_firstname);
+    userInfoSquare.appendChild(p);
+    p2.appendChild(user_lastname);
+    userInfoSquare.appendChild(p2);
+    p3.appendChild(user_email);
+    userInfoSquare.appendChild(p3);
+    p4.appendChild(user_phone);
+    userInfoSquare.appendChild(p4);
+
+
+    //For tag/posts purposes
+    let user_selling_list = document.createTextNode(result.Item.ListofProductIDSelling);
+    let user_sold_list = document.createTextNode(result.Item.ListofProductIDSold);
+    let user_user_id = document.createTextNode(result.Item.UserID);
+    
+    
+
+
 }
